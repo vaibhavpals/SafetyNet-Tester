1632bd47fefda869eebe53707faf60b8f0efebfa
vaibhavpals <vaibhavpalsingh@gmail.com>
2016-12-04 10:29:55 +0530

    Added nonce and safety net call code

--- app/src/main/java/com/vpals/apps/safetynettestapp/MainActivity.java
+++ app/src/main/java/com/vpals/apps/safetynettestapp/MainActivity.java
@@ -1,31 +1,46 @@
 package com.vpals.apps.safetynettestapp;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.safetynet.SafetyNet;
+import com.google.android.gms.safetynet.SafetyNetApi;
 
+import java.security.SecureRandom;
+
+public class MainActivity extends AppCompatActivity
+        implements ConnectionCallbacks, OnClickListener {
+
+    GoogleApiClient mGoogleApiClient;
+    Button btnCheck;
+    int counter = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(SafetyNet.API)
+                .addConnectionCallbacks(this)
+                .build();
+        btnCheck = (Button) findViewById(R.id.btnCheck);
+        btnCheck.setOnClickListener(this);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
@@ -37,9 +52,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
@@ -48,5 +60,46 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        byte[] nonce = getRequestNonce();
+        SafetyNet.SafetyNetApi.attest(mGoogleApiClient, nonce)
+                .setResultCallback(new ResultCallback<SafetyNetApi.AttestationResult>() {
+                    @Override
+                    public void onResult(SafetyNetApi.AttestationResult result) {
+                        Status status = result.getStatus();
+                        if (status.isSuccess()) {
+                            Log.i("Safety Net Result", result.getJwsResult());
+                        } else {
+                            Log.i("Safety Net Error", "Oops!");
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i("Safety Net Error", "Connection Suspended");
+    }
+
+    public byte[] getRequestNonce() {
+        SecureRandom random = new SecureRandom();
+        byte bytes[] = new byte[16];
+        random.nextBytes(bytes);
+        Log.i("Nonce size", "length :" + bytes.length);
+        Log.i("Nonce generated: ", bytes.toString());
+        return bytes;
+    }
+
+    @Override
+    public void onClick(View v) {
+        counter++;
+        if (v.getId() == R.id.btnCheck && counter % 2 != 0) {
+            mGoogleApiClient.connect();
+        }
+        else
+            mGoogleApiClient.disconnect();
     }
 }
